5b207fea3fe6124d5f1d9cdbd1345773f1372887
Christopher Pham <chrismpham@gmail.com>
2019-08-28 15:12:35 -0700

    fixed  CSS underline transitions in a tags

--- client/src/components/react-icons/icons.jsx
+++ client/src/components/react-icons/icons.jsx
@@ -3,18 +3,18 @@ import { FaLinkedin, FaBattleNet, FaGithub } from 'react-icons/fa';
 
 const Icons = (props) =>
   <div className="icon-content">
-    <div className="icon-item">
-      <a href="http://linkedin.com/in/chrismpham/" target="_blank">
+    <div className="icon-item 1">
+      <a href="http://linkedin.com/in/chrismpham/" className="active" target="_blank">
         <FaLinkedin />
       </a>
     </div>
-    <div className="icon-item">
-      <a href="">
+    <div className="icon-item 2">
+      <a href="" className="active">
         <FaBattleNet />
       </a>
     </div>
-    <div className="icon-item">
-      <a href="">
+    <div className="icon-item 3">
+      <a href="" className="active">
         <FaGithub />
       </a>
     </div>
